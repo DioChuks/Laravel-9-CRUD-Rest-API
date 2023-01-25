9765756cd19b280e063828fe43488cb4c426761f
Liam Dio <44742525+DioChuks@users.noreply.github.com>
2023-01-25 11:00:24 +0000

    remove platform check

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,8 +22,6 @@ class ComposerAutoloaderInitc3fda4d090ad6372c92dfbae29ec2684
             return self::$loader;
         }
 
-        require __DIR__ . '/platform_check.php';
-
         spl_autoload_register(array('ComposerAutoloaderInitc3fda4d090ad6372c92dfbae29ec2684', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(__DIR__));
         spl_autoload_unregister(array('ComposerAutoloaderInitc3fda4d090ad6372c92dfbae29ec2684', 'loadClassLoader'));
